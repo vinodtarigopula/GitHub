454e780ad7ca90d80398b8cc39c1feda90a8f105
vinodtarigopula <vinodkumartarigopula2@gmail.com>
2023-03-08 08:46:56 -0400

    Daily laws

---
+++ codingbat/src/warmup1/SleepinClass.java
@@ -0,0 +1,18 @@
+package warmup1;
+
+public class SleepinClass {
+    public static void main(String[] args){
+
+        sleepIn(true, true);
+        sleepIn(true, false);
+        sleepIn(false, true);
+        sleepIn(false, false);
+}
+    //The parameter weekday is true if it is a weekday,and the parameter vacation is true if we are on vacation.
+   // We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+    {
+        public static boolean sleepIn ( boolean weekday, boolean vacation)
+
+    }
+}
---
+++ practice2/src/ArrayExample.java
@@ -0,0 +1,12 @@
+public class ArrayExample {
+    public static void main(String []args) {
+
+        int a[] = {1, 2, 4, 6, 9, 7};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+
+        }
+    }
+
+}
